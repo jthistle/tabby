b892863d7bf43202bac73c3cdf698e614f046237
James Thistlewood <jamesthistlewood@gmail.com>
2020-07-13 19:57:18 +0100

    fix crash on undo resize bar

--- src/lib/bar.py
+++ src/lib/bar.py
@@ -20,12 +20,9 @@ class Bar:
     def change_size(self, mult):
         """If >1, mult must be an int. If <1, mult must be a rational number in the form 1/n, where n is an int."""
         if mult > 1:
-            # logger.info("Initial size: {}".format(self.size()))
-            # logger.info("{} to {}, step {}".format(1, self.size() * 2, mult))
+            mult = int(mult)
             for i in range(1, self.size() * mult, mult):
-                # logger.info("{}".format(i))
                 self.chords.insert(i, Chord(self))
-            # logger.info("Final size: {}".format(self.size()))
         elif mult < 1:
             spacing = int(1 / mult)
             for i in range(self.size() - 1, spacing - 2, -spacing):
